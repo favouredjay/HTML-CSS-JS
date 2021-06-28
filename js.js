f636afa964b5c3aabfc6445e2e6913e1f98c96cd
favouredjay <judom.2011@gmail.com>
2021-06-28 21:52:54 +0100

    JS class work

---
+++ js.js
@@ -0,0 +1,46 @@
+var randomNumber = Math.floor(Math.random() *13);
+console.log(randomNumber)
+
+var number = prompt("Enter number")
+var day = parseInt(number)
+switch(day){
+    case 1: 
+        console.log( "Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log(output = "Tuesday");
+        break;
+    case 4:
+        console.log(output = "Wednesday");
+        break;
+    case 5:
+        console.log(output = "Thursday");
+        break;
+    case 6:
+        console.log(output = "Friday");
+        break;
+    case 7:
+        console.log(output = "Saturday");
+        break;                    
+}
+
+var i = 0;
+var counter = 1;
+do{
+    counter *=2;
+    i++;
+}
+while (counter <= 5000);
+console.log(i)
+
+ 
+
+     if (counter == 1){
+         console.log(i)
+     }
+     
+    }
+
---
+++ prime.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <= 10; i++){
+    var counter = 0;
+    for(let j = 2; j <=i ; j++){
+    if(i%j == 0){
+    counter++;
+        // counter++;
+    
+    }
+}
+}
